970ebcfa27cdf853b1c041cddfa1ad5839735a6d
uabdm <50030989+uabdm@users.noreply.github.com>
2019-04-29 08:56:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function execute() {
   })
       .then(function(response) {
             // Handle the results here (response.result has the parsed body).
-            console.log("Response", response.containerId);
+            console.log("Response", response);
             },
             function(err) { console.error("Execute error", err); });
  }
