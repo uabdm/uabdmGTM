46a3a08161feea89433140318a15cb0d313ebd22
uabdm <50030989+uabdm@users.noreply.github.com>
2019-04-29 09:04:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ function execute() {
       .then(function(response) {
             // Handle the results here (response.result has the parsed body).
             console.log("Response", response);
-            const getContainers = response.container.containerId;
+            const getContainers = response.result.container;
             console.log(getContainers);
             },
             function(err) { console.error("Execute error", err); });
