ea001bcc7f26f444820c3c49f8686043d7a37a99
uabdm <50030989+uabdm@users.noreply.github.com>
2019-05-01 14:41:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -61,8 +61,6 @@ function getWorkspaceID(containerID) {
       })
           .then(function(response) {
                   // Handle the results here (response.result has the parsed body).
-                  let triggers = getTriggers();
-                  console.log("Triggers " + triggers);
                   let workspaceIDs = response.result.workspace[0].workspaceId;
                   console.log("Get Workspace Response ", response);
                   console.log("Workspace ID " + workspaceIDs);
@@ -97,6 +95,7 @@ function getWorkspaceID(containerID) {
 }
 */
 
+// Get a list of every trigger from the Cookie Consent Container
 function getTriggers() {
   return gapi.client.tagmanager.accounts.containers.workspaces.triggers.list({
     "parent": "accounts/4701785906/containers/11828399/workspaces/8"
@@ -104,6 +103,46 @@ function getTriggers() {
       .then(function(response) {
               // Handle the results here (response.result has the parsed body).
               console.log("Get Triggers Response", response);
+            },
+            function(err) { console.error("Execute error", err); });
+}
+
+// Create each trigger individually from the list retrieved from the getTriggers function
+function createTriggers() {
+  return gapi.client.tagmanager.accounts.containers.workspaces.triggers.create({
+    "parent": "accounts/4701785906/containers/11714726/workspaces/8",
+    "resource": {
+      "path": "accounts/4701785906/containers/11828399/workspaces/8/triggers/5",
+      "accountId": "4701785906",
+      "containerId": "11828399",
+      "workspaceId": "8",
+      "triggerId": "5",
+      "name": "Cookie Consent Marketing",
+      "type": "customEvent",
+      "customEventFilter": [
+        {
+          "type": "equals",
+          "parameter": [
+            {
+              "type": "template",
+              "key": "arg0",
+              "value": "{{_event}}"
+            },
+            {
+              "type": "template",
+              "key": "arg1",
+              "value": "cookieconsent_marketing"
+            }
+          ]
+        }
+      ],
+      "fingerprint": "1556734655544",
+      "tagManagerUrl": "https://tagmanager.google.com/#/container/accounts/4701785906/containers/11828399/workspaces/8/triggers/5?apiLink=trigger"
+    }
+  })
+      .then(function(response) {
+              // Handle the results here (response.result has the parsed body).
+              console.log("Create Triggers ", response);
             },
             function(err) { console.error("Execute error", err); });
 }
