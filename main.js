38079ab41e1c957792bd4791ff188b6af71cadfc
uabdm <50030989+uabdm@users.noreply.github.com>
2019-05-02 09:19:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ function getTriggers(containerIDs, workspaceIDs) {
               console.log("Container IDs passed through to GetTriggers function is " + containerIDs);
               console.log("Workspace IDs passed through to GetTriggers function is " + workspaceIDs);
               let triggers = response.result.trigger;
-              console.log("Triggers " + triggers);
+              console.log("Triggers ", triggers);
               for (let i = 0; i < triggers.length; i+=1) {
                 let path = triggers[i].path;
                 let containerID = triggers[i].containerId;
