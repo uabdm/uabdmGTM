3a3e7d080d433c611397635804373f8ec4cc5534
uabdm <50030989+uabdm@users.noreply.github.com>
2019-05-04 16:14:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -125,7 +125,7 @@ function getTriggers(containerIDs, workspaceIDs) {
                 console.log(value);
                 console.log("Container IDs passed through to GetTriggers function is " + containerIDs);
                 console.log("Workspace IDs passed through to GetTriggers function is " + workspaceIDs);
-                createTriggers(containerIDs, workspaceIDs, path, containerID, workspaceID, triggerID, triggerName, fingerPrint, tagManagerUrl, value);
+                //createTriggers(containerIDs, workspaceIDs, path, containerID, workspaceID, triggerID, triggerName, fingerPrint, tagManagerUrl, value);
               //}
             }
             },
