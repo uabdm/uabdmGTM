0f0a63e6ff5b9e8b1af68f717486f95443531735
uabdm <50030989+uabdm@users.noreply.github.com>
2019-06-10 09:28:42 -0500

    Update vimeo.js

--- vimeo.js
+++ vimeo.js
@@ -59,7 +59,7 @@ function getWorkspaceID(containerID) {
                   console.log("Workspace ID " + workspaceIDs);
                   // createFolders(containerIDs, workspaceIDs);
                   //getTriggers(containerIDs, workspaceIDs);
-                  getVariables(containerIDs, workspaceIDs);
+                  //getVariables(containerIDs, workspaceIDs);
                   //getTags(containerIDs, workspaceIDs);
                 },
                 function(err) { console.error("Execute error", err); });
